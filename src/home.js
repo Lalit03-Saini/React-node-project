bac4ae7c309c5df0e7347c2b201b19f6cd31cfcd
Lalit03-Saini <lalitdaiya54@gmail.com>
2022-11-13 18:49:21 +0530

    #

--- src/home.js
+++ src/home.js
@@ -16,17 +16,17 @@ const Home = () => {
     };
     console.log(navigator);
 
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlide");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        myIndex++;
-        if (myIndex > x.length) { myIndex = 1 }
-        x[myIndex - 1].style.display = "block";
-        setTimeout(carousel, 2000);
-    }
+    // function carousel() {
+    //     var i;
+    //     var x = document.getElementsByClassName("mySlide");
+    //     for (i = 0; i < x.length; i++) {
+    //         x[i].style.display = "none";
+    //     }
+    //     myIndex++;
+    //     if (myIndex > x.length) { myIndex = 1 }
+    //     x[myIndex - 1].style.display = "block";
+    //     setTimeout(carousel, 2000);
+    // }
     return (
         <>
             <div className="gallery">
