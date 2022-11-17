10151b5ca284f36a66ed73ebc72d4fcbd8744f64
Lalit03-Saini <lalitdaiya54@gmail.com>
2022-11-17 16:21:03 +0530

    "

--- src/home.js
+++ src/home.js
@@ -11,7 +11,7 @@ const Home = () => {
     useEffect(() => {
         setLoader(true)
         setTimeout(() => {
-            setLoader(true)
+            setLoader(false)
         }, 3000)
     }, [])
 
