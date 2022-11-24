401e20dba6766adba8a5e598d3c069fd99253ca2
Lalit Saini <112317285+Lalit03-Saini@users.noreply.github.com>
2022-11-24 15:36:12 +0530

    Changing Image url

--- src/silder-date.js
+++ src/silder-date.js
@@ -1,19 +1,19 @@
 
 export const sliderDate = [
     {
-        image: "https://willowy-halva-53bf86.netlify.app/static/media/Road.6c3b96ac47abf0c3545f.jpg",
+        image: "https://react-blog-app03.netlify.app/static/media/Road.6c3b96ac47abf0c3545f.jpg",
         heading: "Title of vertical gallery",
         desc: "trevel/August21 2017",
         id: 0
     },
     {
-        image1: "https://willowy-halva-53bf86.netlify.app/static/media/mountain.735e63fecaf4299bda3b.jpg",
+        image1: "https://react-blog-app03.netlify.app/static/media/mountain.735e63fecaf4299bda3b.jpg",
         heading1: "The sound cloud you loved",
         desc1: "trevel/August21 2017",
         id: 1
     },
     {
-        image2: "https://willowy-halva-53bf86.netlify.app/static/media/lakenature.e7c6b511b2d9fc8cf50f.jpg",
+        image2: "https://react-blog-app03.netlify.app/static/media/lakenature.e7c6b511b2d9fc8cf50f.jpg",
         heading2: "Nature Cloud is doomed",
         desc2: "trevel/August21 2017",
         id: 2
